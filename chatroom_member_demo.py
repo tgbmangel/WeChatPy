ee1aecaebf9f095e4f5e578e1b486e6d21c4817c
zhu <tgbmangel@163.com>
2018-09-04 17:22:56 +0800

    获取群内成员的信息，写到csv中

--- chatroom_member_demo.py
+++ chatroom_member_demo.py
@@ -7,6 +7,7 @@
 
 import itchat
 from itchat.content import *
+import csv
 
 def get_chatroom_username(room_name):
     try:
@@ -16,19 +17,25 @@ def get_chatroom_username(room_name):
         return
 
 
+
 itchat.auto_login(hotReload=True)
 memberList = itchat.update_chatroom(get_chatroom_username('13栋业主群'), detailedMember=True)
 print(len(memberList))
 print(memberList)
+csvFile2 = open('csvFile2.csv','w', newline='') # 设置newline，否则两行之间会空一行
+writer = csv.writer(csvFile2)
+
 for x,y in memberList.items():
-    # print(x,y)
+    print(x,y)
     n=0
     if x=='MemberList':
         for z in y:
             # print(z['NickName'])
+            writer.writerow([z['UserName'],z['Sex'],z['Province'],z['City']])
             n += 1
             for key,value in z.items():
 
                 print(key,value)
             print('--'*10)
         print(n)
+csvFile2.close()
