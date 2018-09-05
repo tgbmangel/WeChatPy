5d3f90a674e956c331a8de1af463af18f5537bad
zhu <tgbmangel@163.com>
2018-09-05 17:21:09 +0800

    优化代码结构和业务

--- Orange.py
+++ Orange.py
@@ -15,9 +15,11 @@ orange_msg_type = {
     3:'地址',
     4:'查询'
 }
+MSG_TURN={}
+orange_info='正宗石门柑橘，自家种的，一件10斤50元包邮。目前是特早熟，现摘现卖。'
+main_wechat_name='西边有片云'
 
 def get_content(content):
-    print('content',content)
     #替换掉收到的信息中的一些符号
     string_c = re.sub("[\s+_,$%^*(\"\']+|[+—！，。？、~@#￥%&*（）]+", "",content)
     org_compile=re.compile(r"^我要买橘子(.*)")
@@ -42,10 +44,9 @@ def get_content(content):
         return -1
 
 def send_msg_to_man(message):
-    a=itchat.search_friends(name='西边有片云')
+    a=itchat.search_friends(name=main_wechat_name)
     itchat.send_msg(message,toUserName=a[0]['UserName'])
 
-msg_turn={}
 def parse_msg_turn(msg_t):
     msg_toman = ''
     for x,y in msg_t.items():
@@ -54,10 +55,12 @@ def parse_msg_turn(msg_t):
             # msg_toman += key
             print(key,value)
             msg_toman += '{},'.format(value)
-    print('msg:',msg_toman)
     return msg_toman
 
 def unicode_nickname(input_string):
+    '''
+    写文件或者存数据库时，表情无法存进去，此函数处理
+    '''
     strrr=ascii(input_string)
     b=''
     if 'U000' in strrr:
@@ -75,45 +78,45 @@ def unicode_nickname(input_string):
     else:
         return input_string
 
-orange_info='正宗石门柑橘，自家种的，一件10斤50元。目前是特早熟，现摘现卖。'
-
 @itchat.msg_register(FRIENDS)
 def add_friend(msg):
     msg.user.verify()
-
+    print('{}添加成功'.format(msg.user))
     msg.user.send('亲！您好！发送："我要买橘子"，将有客服为您转接！\n{}'.format(orange_info))
 
 @itchat.msg_register([TEXT])
 def text_reply(msg):
-    print(msg.text)
+    # print(msg['MsgType'])
     if msg.text:
-        print(msg)
-        global msg_turn
-        msg_nick_name=unicode_nickname(msg['User']['NickName'])
-        if msg_nick_name in msg_turn.keys():
-            pass
-        else:
-            msg_turn[msg_nick_name]={}
-        wechat_content = msg.text
-        msg_orange=get_content(wechat_content)
-        if msg_orange==1:
-            msg.user.send('亲，请说明要多少，目前1件10斤，50元，发送格式：【我要订+几件】')
-        elif msg_orange==2:
-            msg_turn[msg_nick_name]['dingdan']=wechat_content
-            print(msg_turn)
-            msg.user.send('已收到:{}'.format(msg.text))
-            msg.user.send('亲，把您的收件人名、地址和联系电话发给我哦。格式：【配送信息+收件人姓名，地址，联系电话】')
-        elif msg_orange==3:
-            msg_turn[msg_nick_name]['dizhi']=wechat_content
-            print(msg_turn)
-            msg.user.send('亲，已收到:{} \n---\n谢谢，稍后会联系您。[愉快]'.format(msg.text))
-            send_msg_to_man(parse_msg_turn(msg_turn))
-        elif msg_orange==4:
-            msg.user.send(parse_msg_turn(msg_turn))
-        elif msg_orange==-1:
-            msg.user.send('谢谢关注，买橘子请发：“我要买橘子”。。\n{}'.format(orange_info))
-
-
-itchat.auto_login(True)
-itchat.run()
+        # print(msg)
+        global MSG_TURN
+        try:
+            msg_nick_name=unicode_nickname(msg['User']['NickName'])
+            if msg_nick_name in MSG_TURN.keys():
+                pass
+            else:
+                MSG_TURN[msg_nick_name]={}
+            wechat_content = msg.text
+            msg_orange=get_content(wechat_content)
+            if msg_orange==1:
+                msg.user.send('亲，请说明要多少，目前1件10斤，50元，发送格式：【我要订+几件】')
+            elif msg_orange==2:
+                MSG_TURN[msg_nick_name]['dingdan']=wechat_content
+                # print(msg_turn)
+                msg.user.send('已收到:{}'.format(msg.text))
+                msg.user.send('亲，把您的收件人名、地址和联系电话发给我哦。格式：【配送信息+收件人姓名，地址，联系电话】')
+            elif msg_orange==3:
+                MSG_TURN[msg_nick_name]['dizhi']=wechat_content
+                # print(msg_turn)
+                msg.user.send('亲，已收到:{} \n---\n谢谢，稍后会联系您。[愉快]'.format(msg.text))
+                send_msg_to_man(parse_msg_turn(MSG_TURN))
+            elif msg_orange==4:
+                msg.user.send(parse_msg_turn(MSG_TURN))
+            elif msg_orange==-1:
+                msg.user.send('谢谢关注，买橘子请发：“我要买橘子”。。\n{}'.format(orange_info))
+        except Exception as e:
+            print('异常打印：{}:{}'.format(e,msg))
+if __name__=='__main__':
+    itchat.auto_login(True)
+    itchat.run()
 
